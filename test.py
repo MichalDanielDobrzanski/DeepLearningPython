021657efa2f874453ebf94ca1598b38add9b7dc5
Michał Dobrzański <dobrzanski.michal.daniel@gmail.com>
2016-07-30 18:08:54 +0200

    Updated readme + 2nd example from 3rd chapter

--- test.py
+++ test.py
@@ -20,21 +20,33 @@
 
 import mnist_loader
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+training_data = list(training_data)
 
 # ---------------------
 # - network.py example:
 
-# import network
-# net = network.Network([784, 30, 10])
-# net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+import network
+net = network.Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 
 # ----------------------
 # - network2.py example:
 
-import network2
-net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
-#net.large_weight_initializer()
-net.SGD(training_data, 30, 10, 0.1, lmbda = 5.0,evaluation_data=validation_data, monitor_evaluation_accuracy=True)
+# import network2
+# net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+# #net.large_weight_initializer()
+# net.SGD(training_data, 30, 10, 0.1, lmbda = 5.0,evaluation_data=validation_data,
+#     monitor_evaluation_accuracy=True)
+
+
+# chapter 3 - Overfitting and regularization example
+# import network2
+# net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+# net.large_weight_initializer()
+# net.SGD(training_data[:1000], 400, 10, 0.5, evaluation_data=test_data,
+#     monitor_evaluation_accuracy=True,
+#     monitor_training_cost=True)
+
 
 # ----------------------
 # - network3.py example:
